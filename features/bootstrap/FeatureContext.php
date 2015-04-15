5bc0ee01832b99c74aa86d81955072c0278a32b0
Dave Hulbert <dave@wearebase.com>
2015-04-15 08:47:03 +0100

    Include behat

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,26 @@
+<?php
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+require_once __DIR__ . '/../../vendor/autoload.php'; // TODO: move this to behat.yml
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context, SnippetAcceptingContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+}
---
+++ features/foo.feature
@@ -0,0 +1,9 @@
+Feature: Foo
+  As a Foo
+  I want to Bar
+  So I can Baz
+
+  Scenario: General foo
+    Given: A new Foo
+    When I Bar
+    Then I should get Baz
